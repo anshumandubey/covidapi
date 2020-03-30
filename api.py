0069fae7349f32ade090688d81f7b208b0bc5354
Anshuman Dubey <adubey@bravurasolutions.com>
2020-03-30 10:47:14 +0530

    Refresh issue fixed

--- api.py
+++ api.py
@@ -4,10 +4,28 @@ import pandas as pd
 import json
 import requests
 import numpy as np
+import time,threading
+from threading import Timer
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+class setInterval :
+    def __init__(self,interval,action) :
+        self.interval=interval
+        self.action=action
+        self.stopEvent=threading.Event()
+        thread=threading.Thread(target=self.__setInterval)
+        thread.start()
+
+    def __setInterval(self) :
+        nextTime=time.time()+self.interval
+        while not self.stopEvent.wait(nextTime-time.time()) :
+            nextTime+=self.interval
+            self.action()
+
+    def cancel(self) :
+        self.stopEvent.set()
 
 def flatten_json(y):
     out = {}
@@ -25,20 +43,28 @@ def flatten_json(y):
     flatten(y)
     return out
 
-resp = requests.get(f"https://pomber.github.io/covid19/timeseries.json")
-data = json.loads(resp.text)
 df = pd.DataFrame(columns=['country', 'date', 'confirmed', 'deaths', 'recovered'])
 df_totalCases = pd.DataFrame(columns=['country', 'date', 'confirmed', 'deaths', 'recovered'])
-for key,value in data.items():
-    dic_flattened = (flatten_json(d) for d in value)
-    temp_df = pd.DataFrame(dic_flattened)
-    temp_df.insert(0, 'country', key)
-    df = df.append([temp_df],ignore_index=True)
-    df_totalCases = df_totalCases.append([temp_df.tail(1)],ignore_index=True)
 
-df['country'] = df['country'].str.lower()
-df_totalCases['country'] = df_totalCases['country'].str.lower()
+def data():
+    global df,df_totalCases
+    print("Updating data...")
+    resp = requests.get(f"https://pomber.github.io/covid19/timeseries.json")
+    data = json.loads(resp.text)
+    df = df.iloc[0:0]
+    df_totalCases = df = df.iloc[0:0]
+    for key,value in data.items():
+        dic_flattened = (flatten_json(d) for d in value)
+        temp_df = pd.DataFrame(dic_flattened)
+        temp_df.insert(0, 'country', key)
+        df = df.append([temp_df],ignore_index=True)
+        df_totalCases = df_totalCases.append([temp_df.tail(1)],ignore_index=True)
 
+    df['country'] = df['country'].str.lower()
+    df_totalCases['country'] = df_totalCases['country'].str.lower()
+
+data()
+inter = setInterval(14400, data)
 
 @app.route('/api/all', methods=['GET'])
 def api_all():
@@ -79,3 +105,5 @@ def api_total():
     else:
         return str(df_total.confirmed)
 
+if __name__ == '__main__':
+    app.run()
--- app.sh
+++ app.sh
@@ -4,7 +4,7 @@ export FLASK_APP=$(pwd)/api.py
 chmod +x api.py
 while true
 do
-    python3 -m flask run &
+    python3 -m flask run --host=0.0.0.0 &
     sleep 4h
     kill -9 $(pgrep -f flask)
     echo "Updating data.."
